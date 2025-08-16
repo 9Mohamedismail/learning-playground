942831bbdf4e9c382ad703a99babbefce407078a
9Mohamedismail <9Mohamedismail@gmail.com>
2025-08-15 22:23:11 -0400

    chat delete socketio

--- LearningSocketIO/learning-socketio/src/App.tsx
+++ LearningSocketIO/learning-socketio/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import { socket } from "./socket";
 
 function App() {
@@ -12,10 +13,17 @@ function App() {
       socket.auth.serverOffset = serverOffset;
     };
 
+    const handleDeleteChat = (serverOffset: number): void => {
+      console.log("wassssup");
+      socket.auth.serverOffset = serverOffset;
+    };
+
     socket.on("chat message", handleSocketMessage);
+    socket.on("chat delete", handleDeleteChat);
 
     return () => {
       socket.off("chat message", handleSocketMessage);
+      socket.off("chat delete", handleDeleteChat);
     };
   }, []);
 
@@ -27,6 +35,18 @@ function App() {
     setCounter(counter + 1);
   };
 
+  const onDelete = () => {
+    axios
+      .get("/DELETE")
+      .then(() => {
+        console.log("hey");
+        socket.emit("chat delete");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="h-screen flex flex-col">
       <div className="p-4">
@@ -35,7 +55,15 @@ function App() {
             <h1 className="border p-2 mb-2"> {message}</h1>
           ))}
       </div>
-
+      <div className="px-2">
+        <button
+          className="border border-black rounded p-4 mb-2"
+          onClick={onDelete}
+        >
+          Clear Chat
+        </button>
+        <h1> {socket.auth.serverOffset}</h1>
+      </div>
       <div className="h-full flex flex-col justify-end">
         <form className="flex" onSubmit={onSubmit}>
           <input
